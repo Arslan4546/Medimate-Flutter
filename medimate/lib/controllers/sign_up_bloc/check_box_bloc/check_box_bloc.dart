f18f72e6aaa594a8e1125c14a23899d6a6c0f37c
Arslan4546 <arslantariq4546@gmail.com>
2025-11-12 12:13:35 +0500

    update check box bloc file

--- medimate/lib/controllers/sign_up_bloc/check_box_bloc/check_box_bloc.dart
+++ medimate/lib/controllers/sign_up_bloc/check_box_bloc/check_box_bloc.dart
@@ -5,8 +5,10 @@ part 'check_box_state.dart';
 
 class CheckBoxBloc extends Bloc<CheckBoxEvent, CheckBoxState> {
   CheckBoxBloc() : super(CheckBoxState(false)) {
-    on<OnClickCheckBox>((event, emit) {
-      emit(CheckBoxState(event.value));
-    });
+    on<OnClickCheckBox>(_onClickCheckBox);
+  }
+  // this is the event handler for checkbox click
+  void _onClickCheckBox(OnClickCheckBox event, Emitter<CheckBoxState> emit) {
+    emit(CheckBoxState(event.value));
   }
 }
