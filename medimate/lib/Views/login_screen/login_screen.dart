81ecfe3f567b54b9a02ff6b4741823066e10a265
Arslan4546 <arslantariq4546@gmail.com>
2025-11-12 12:28:10 +0500

    update

--- medimate/lib/Views/login_screen/login_screen.dart
+++ medimate/lib/Views/login_screen/login_screen.dart
@@ -8,91 +8,112 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var Size(:width,:height)=MediaQuery.sizeOf(context);
+    var Size(:width, :height) = MediaQuery.sizeOf(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sign In",style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.w700
-        ),),
+        title: Text(
+          "Sign In",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+        ),
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.only(left: 40,right: 40),
+        padding: const EdgeInsets.only(left: 40, right: 40),
         child: Column(
           children: [
-            SizedBox(
-              height: 25,
-            ),
+            SizedBox(height: 25),
             SizedBox(
               height: 70,
-              child: CustomTextField(hintText: "Enter your email", prefixIcon: Icons.email_rounded) 
+              child: CustomTextField(
+                hintText: "Enter your email",
+                prefixIcon: Icons.email_rounded,
+              ),
             ),
             Padding(padding: EdgeInsetsGeometry.all(8)),
-            CustomTextField(hintText: "Enter your password", prefixIcon: Icons.lock),
+            CustomTextField(
+              hintText: "Enter your password",
+              prefixIcon: Icons.lock,
+            ),
             Padding(padding: EdgeInsetsGeometry.all(1)),
             Align(
               alignment: Alignment.topRight,
-              child: TextButton(onPressed: (){}, child: Text("Forget Password ?",style: TextStyle(
-                color: Colors.blueAccent
-              ),)),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Forget Password ?",
+                  style: TextStyle(color: Colors.blueAccent),
+                ),
+              ),
             ),
             Padding(padding: EdgeInsetsGeometry.all(25)),
             SizedBox(
               height: 56,
               width: double.infinity,
-              child: CustomButton(buttonColor: Colors.blueAccent, label: "Sign In", labelColorData: Colors.white, onPressed: (){}),
+              child: CustomButton(
+                buttonColor: Colors.blueAccent,
+                label: "Sign In",
+                labelColorData: Colors.white,
+                onPressed: () {},
+              ),
             ),
             Padding(padding: EdgeInsetsGeometry.all(1)),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               spacing: 0,
               children: [
-                Text("Don't have an account?",style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600
-                )),
-                TextButton(onPressed: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => SignUpScreen(),));
-                }, child: Text("Sign Up",style: TextStyle(
-                  color: Colors.blueAccent,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600
-                ),))
+                Text(
+                  "Don't have an account?",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => SignUpScreen()),
+                    );
+                  },
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
               ],
             ),
-            Padding(padding: EdgeInsetsGeometry.all(height*0.05)),
+            Padding(padding: EdgeInsetsGeometry.all(height * 0.05)),
             Row(
               children: [
                 Expanded(
                   flex: 4,
-                  child:Container(
-                    height: 1,
-                    color: Colors.black26,
-                  ),
+                  child: Container(height: 1, color: Colors.black26),
                 ),
                 Expanded(
                   flex: 1,
-                  child:Center(child: Text("OR",style: TextStyle(fontWeight: FontWeight.w500,color: Colors.black26),))
+                  child: Center(
+                    child: Text(
+                      "OR",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black26,
+                      ),
+                    ),
+                  ),
                 ),
                 Expanded(
                   flex: 4,
-                  child:Container(
-                    height: 1,
-                    color: Colors.black26,
-                  ), 
-                )
+                  child: Container(height: 1, color: Colors.black26),
+                ),
               ],
             ),
-            SizedBox(
-              height: height*0.05,
-            ),
+            SizedBox(height: height * 0.05),
             Container(
               height: 56,
               width: double.infinity,
               decoration: BoxDecoration(
-                border: Border.all(width: 1,color: Colors.black54),
-                borderRadius: BorderRadius.circular(5)
+                border: Border.all(width: 1, color: Colors.black54),
+                borderRadius: BorderRadius.circular(5),
               ),
               child: Center(
                 child: Row(
@@ -102,12 +123,19 @@ class LoginScreen extends StatelessWidget {
                     SizedBox(
                       height: 20,
                       width: 20,
-                      child: Image.asset("assets/google-logo.png")),
-                    Text("Sign in with Google",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w600),)
+                      child: Image.asset("assets/google-logo.png"),
+                    ),
+                    Text(
+                      "Sign in with Google",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ],
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
