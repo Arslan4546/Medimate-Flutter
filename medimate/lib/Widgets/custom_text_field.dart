0ee616b3aae53510bb71127c22892bdebe88c84d
Arslan4546 <arslantariq4546@gmail.com>
2025-11-20 01:59:22 +0500

    update

--- medimate/lib/Widgets/custom_text_field.dart
+++ medimate/lib/Widgets/custom_text_field.dart
@@ -3,25 +3,28 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final String hintText;
   final IconData prefixIcon;
-  const CustomTextField({super.key, required this.hintText, required this.prefixIcon});
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    required this.prefixIcon,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-                decoration: InputDecoration(
-                  hintText: hintText,
-                  fillColor: Color.fromARGB(255, 250, 251, 249),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.transparent),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  
-                  prefixIcon:Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Icon(prefixIcon),
-                  ),  
-                  
-                ),
-              );
+      decoration: InputDecoration(
+        hintText: hintText,
+        fillColor: Color.fromARGB(255, 250, 251, 249),
+        border: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.transparent),
+          borderRadius: BorderRadius.circular(5),
+        ),
+
+        prefixIcon: Padding(
+          padding: const EdgeInsets.only(left: 10),
+          child: Icon(prefixIcon),
+        ),
+      ),
+    );
   }
 }
